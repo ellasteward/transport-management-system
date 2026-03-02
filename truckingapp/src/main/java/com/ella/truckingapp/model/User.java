7788a4679494731972238fd1de9bca7ca6568e32
Ella Steward <ellasteward-ellarose.nz@gmail.com>
2026-03-03 11:21:09 +1300

    package name fixed User.java

--- truckingapp/src/main/java/com/ella/truckingapp/model/User.java
+++ truckingapp/src/main/java/com/ella/truckingapp/model/User.java
@@ -1,4 +1,4 @@
-package model;
+package com.ella.truckingapp.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
---
+++ truckingapp/src/main/java/com/ella/truckingapp/respository/UserRespository.java
@@ -0,0 +1,4 @@
+package com.ella.truckingapp.respository;
+
+public class UserRespository {
+}
