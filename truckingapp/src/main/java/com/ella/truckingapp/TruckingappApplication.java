69edff24e99fd2c36a27224cbd3d14208635f134
Ella Steward <ellasteward-ellarose.nz@gmail.com>
2026-03-02 15:58:34 +1300

    Initial Spring Boot project setup with MySQL configuration

---
+++ truckingapp/src/main/java/com/ella/truckingapp/TruckingappApplication.java
@@ -0,0 +1,13 @@
+package com.ella.truckingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckingappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TruckingappApplication.class, args);
+    }
+
+}
---
+++ truckingapp/src/test/java/com/ella/truckingapp/TruckingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ella.truckingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruckingappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
